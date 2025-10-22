522d858ed1eb6dbe25f41b72603bb67171097e03
w1ndrunn3rr <w1ndrunn3rr@gmail.com>
2025-10-22 18:38:23 +0000

    fe expand

--- src/models/features_extractor.py
+++ src/models/features_extractor.py
@@ -11,18 +11,39 @@ class FeaturesExtractor(L.LightningModule):
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            # Layer 2
             nn.MaxPool2d(3, stride=2, padding=1),
+            # Layer 2
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            # Layer 3
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            # Layer 3
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            # Ending layers
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            # Layer 4
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            # Layer 5
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            # Ending layers
             nn.AdaptiveAvgPool2d((1, 1)),
         )
 
